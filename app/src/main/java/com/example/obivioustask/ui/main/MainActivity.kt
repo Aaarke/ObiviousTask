513c9beafd34cc73c792bc08b75f4d8f19ca192a
Roshan kumar <roshankumarroshi@gmail.com>
2020-02-25 21:02:51 +0530

    first commit

---
+++ app/src/main/java/com/example/obivioustask/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.obivioustask.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.obivioustask.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
---
+++ app/src/main/java/com/example/obivioustask/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.obivioustask.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
